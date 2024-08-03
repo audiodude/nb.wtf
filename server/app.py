03e6be4d4ebb4b49923d1e6763ccf071334d0713
Travis Briggs <audiodude@gmail.com>
2024-08-03 09:56:25 -0700

    Initial commit, mostly working

---
+++ server/app.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+from bs4 import BeautifulSoup
+import flask
+from pymongo import MongoClient
+import requests
+
+app = flask.Flask(__name__)
+MONGODB_URI = os.environ['MONGODB_URI']
+PAGE_URL = os.environ['PAGE_URL']
+USER_AGENT = 'nb-wtf/1.0 <audiodude@gmail.com>'
+
+def update_db(mapping):
+  client = MongoClient(MONGODB_URI)
+  client.nbwtf.links.drop()
+
+  for slug, url in mapping.items():
+    client.nbwtf.links.insert_one({'slug': slug, 'url': url})
+
+@app.route('/api/v1/on_update')
+def update():
+  resp = requests.get(PAGE_URL, headers={'User-Agent': USER_AGENT})
+  resp.raise_for_status()
+
+  soup = BeautifulSoup(resp.text, 'html.parser')
+
+  mapping = {}
+  table = soup.find('table', {'class': 'wikitable'})
+  for row in table.find_all('tr'):
+    cols = row.find_all('td')
+    if not cols:
+      continue
+    slug, url = cols[0].text, cols[1].text
+    mapping[slug] = url
+
+  update_db(mapping)
+
+  return 'Updated!'
+
+  @app.route('/<slug>')
+  def redirect(slug):
+    print(slug)
+    client = MongoClient(MONGODB_URI)
+    link = client.nbwtf.links.find_one({'slug': slug})
+    return flask.redirect(link['url'])
